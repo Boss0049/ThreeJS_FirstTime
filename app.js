24998beacd6ff8e72a678caa998339e61d959191
bagidea <aodbboy@hotmail.com>
2021-03-12 22:10:33 +0700

    Edit app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3001;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
